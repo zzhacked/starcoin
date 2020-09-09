ac1993abcaaeafc02b7a70cee3cb0ecec2cfd778
Guangyu Zhu <guangyu.zhu@aliyun.com>
2020-09-09 14:28:36 +0800

    wrong token code for test

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -6,7 +6,7 @@ extern crate log;
 pub use block_executor::{block_execute, BlockExecutedData};
 pub use executor::*;
 pub use starcoin_transaction_builder::{
-    build_accept_token_txn, build_transfer_from_association, build_transfer_txn,
+    build_accept_token_txn, build_transfer_from_association, build_transfer_txn, build_transfer_txn_with_wrong_token_code,
     build_transfer_txn_by_token_type, create_signed_txn_with_association_account,
     encode_create_account_script, encode_transfer_script, peer_to_peer_txn_sent_as_association,
     DEFAULT_EXPIRATION_TIME, DEFAULT_MAX_GAS_AMOUNT,
@@ -17,4 +17,4 @@ mod executor;
 #[cfg(test)]
 pub mod executor_test;
 #[cfg(test)]
-pub mod prolog_error_code_test;
+pub mod error_code_test;
